85894b1e1b8d2bbdde332b1e3e0ceccc098ad4cd
Luis María Pizarro <lmpizarro@gmail.com>
2017-10-24 18:15:32 -0300

    optimize fire lammps

---
+++ genEam/optimizeU.py
@@ -0,0 +1,27 @@
+from ase import Atoms
+from ase.calculators.lammps import LAMMPS
+from ase.constraints import UnitCellFilter
+from ase.optimize import FIRE
+
+if __name__ == '__main__':
+
+    pair_style = 'eam/alloy'
+    Pd_eam_file = 'U2.eam'
+    pair_coeff = [ '* * ' + Pd_eam_file + ' U' ]
+    parameters = { 'pair_style' : pair_style, 'pair_coeff' : pair_coeff }
+    files = [ Pd_eam_file ]
+
+    calc = LAMMPS(parameters=parameters, files=files)
+    a0 = 4.20
+    b0 = a0 / 2.0
+    bulk = Atoms(['U']*4,
+                     positions=[(0,0,0),(b0,b0,0),(b0,0,b0),(0,b0,b0)],
+                     cell=[a0]*3,
+                     pbc=True)
+    bulk.set_calculator(calc)
+
+    FIRE(UnitCellFilter(bulk, mask=[1,1,1,0,0,0]), logfile='FIRE.log').run(fmax=0.0001)
+
+    print bulk.get_cell()[0][0]
+
+    calc.clean()
